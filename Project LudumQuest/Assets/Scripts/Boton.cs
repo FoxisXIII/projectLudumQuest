5a341190d20da5e61b6844f1ceeaf77d9d9ea02b
Loquele <joviedo@edu.tecnocampus.cat>
2020-04-18 19:14:34 +0200

    Puetas Hechas con botones

---
+++ Project LudumQuest/Assets/Scripts/Boton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boton : MonoBehaviour
+{
+    public Puerta puerta;
+    public bool abierta = false, bajando;
+    public float speed = 1f;
+    public Vector2 posPressed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        posPressed = new Vector2(transform.position.x, transform.position.y - 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (abierta)
+        {
+            transform.position=Vector2.MoveTowards(transform.position, posPressed, speed*Time.deltaTime);
+                        
+        }
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!abierta)
+            {
+                puerta.abierta = true;
+            }
+        }
+    }
+}
---
+++ Project LudumQuest/Assets/Scripts/Puerta.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puerta : MonoBehaviour
+{
+
+    public bool abierta = false;
+    public float speed = 1f;
+    public Vector2 posPressed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        posPressed = new Vector2(transform.position.x, transform.position.y + 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (abierta)
+        {
+            transform.position=Vector2.MoveTowards(transform.position, posPressed, speed*Time.deltaTime);
+            
+        }
+    }
+    
+    
+    
+}
